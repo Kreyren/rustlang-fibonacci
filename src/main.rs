450e1194205980ff198aa4d26e101aa1c35ae090
Kreyren <KostWarCZE@RiXotStudio.cz>
2020-01-22 21:03:34 +0000

    stub

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
-use std::io;
+use die::die;
+
+use std::{thread, time};
+
+fn fib(n: f64) -> f64 {
+	if n <= 1.0 {
+		return n;
+	} else if n > 1.0 {
+		return fib(n-1.0) + fib(n-2.0);
+	} else {
+		die!(256; "Unexpected happend while processing number '{}'", n);
+	}
+}
 
 fn main() {
-	let mut x = String::new();
-	let mut x = String::new();
+	// Input - FIXME: Implement grabbing from cli
+	let n = 9.0;
 
+	// Core
 	loop {
-		let z = x + y;
-		let x = y;
-		let y = z;
+		println!("{}", fib(n));
 
-		println!("{}", x);
-		if x >= 255 {
-			break
-		}
-	}
+		// Wait 500 ms after each output to avoid flodding the console
+		thread::sleep(time::Duration::from_millis(500));
+	}	
 }
