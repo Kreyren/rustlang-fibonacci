b5bbf7034ac738f77689c634d79999707f058b35
Kreyren <KostWarCZE@RiXotStudio.cz>
2020-02-13 19:45:05 +0000

    fg456fg4j6

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -17,5 +17,5 @@ fn main() {
 		.die_code("Invalid argument 'fixme_argument' has been parsed", 2);
 
 	// FIXME: Output based on userinput
-	println!("{}", fibonacci().take(n);
+	println!("{}", fibonacci().take(n));
 }
