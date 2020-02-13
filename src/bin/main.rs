cb8bc1de808057cd77796ccd007a74d320562ef6
Kreyren <KostWarCZE@RiXotStudio.cz>
2020-02-13 20:03:38 +0000

    sagasg

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,6 @@
 use std::string::String;
 use std::io;
 use die::Die;
-use std::convert::TryInto;
 
 use kreybonacci::fibonacci;
 use kreybonacci::fixme;
@@ -14,9 +13,9 @@ fn main() {
 	fixme!("Output helpful warning for fixme_argument_parsed which requires contrib in die crate");
 	io::stdin().read_line(&mut userInput)
 		.die_code("Unexpected input 'fixme_argument_parsed' has been parsed", 2);
-	let n: u128 = userInput.trim_end_matches("\n").parse()
+	let n: usize = userInput.trim_end_matches("\n").parse()
 		.die_code("Invalid argument 'fixme_argument' has been parsed", 2);
 
 	// FIXME: Output based on userinput
-	println!("{:?}", fibonacci().take(n.try_into().unwrap()));
+	println!("{:?}", fibonacci().take(n).collect::<Vec<_>>());
 }
