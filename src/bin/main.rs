2a73daad1df914776753f729ea5930f3582988e8
Kreyren <KostWarCZE@RiXotStudio.cz>
2020-02-13 19:40:30 +0000

    fdfdgj

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -13,9 +13,9 @@ fn main() {
 	fixme!("Output helpful warning for fixme_argument_parsed which requires contrib in die crate");
 	io::stdin().read_line(&mut userInput)
 		.die_code("Unexpected input 'fixme_argument_parsed' has been parsed", 2);
-	let n: u64 = userInput.trim_end_matches("\n").parse()
+	let n: u128 = userInput.trim_end_matches("\n").parse()
 		.die_code("Invalid argument 'fixme_argument' has been parsed", 2);
 
 	// FIXME: Output based on userinput
-	println!("{:?}", fibonacci().take(n);
+	println!("{}", fibonacci().take(n);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+// Macro to handle fixme messages
+// FIXME: Implement export in log directory based on system used
+// FIXME: Allow end-user to customize the message
+// FIXME: Allow end-user to mute the messages through configuration
+// FIXME: Contribute in log to outsource maintainance on them
+#[macro_export]
+macro_rules! fixme {
+	($msg:expr) => ( println!("FIXME: {}", $msg);)
+}
+
 // NOTICE: Has to be inlined, because inlining does not happend across crate boundaries unless inline is set (difference of +-70% of efficiency)
 #[inline]
 pub fn fibonacci() -> impl Iterator<Item = u128> {
@@ -12,14 +22,4 @@ pub fn fibonacci() -> impl Iterator<Item = u128> {
 //#[cfg(test)]
 //if fibonacci(5) != "38" {
 //	die(1; "Test for fibonacci(5) did not return 38, returned '{}'", fibonacci(5));
-//}
-
-// Macro to handle fixme messages
-// FIXME: Implement export in log directory based on system used
-// FIXME: Allow end-user to customize the message
-// FIXME: Allow end-user to mute the messages through configuration
-// FIXME: Contribute in log to outsource maintainance on them
-#[macro_export]
-macro_rules! fixme {
-	($msg:expr) => ( println!("FIXME: {}", $msg);)
-}
+//};
